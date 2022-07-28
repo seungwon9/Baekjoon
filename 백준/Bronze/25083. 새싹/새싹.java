4c0584560e76ed7a49218fd2f2aac7caf149eded
tlstmddnjs <97091772+tlstmddnjs@users.noreply.github.com>
2022-07-29 00:08:47 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=	"         ,r'\"7\n"+
+					"r`-_   ,'  ,/\n"+
+					" \\. \". L_r'\n"+
+					"   `~\\/\n"+
+					"      |\n"+
+					"      |\n";
+		System.out.println(s);
+	}
+
+}
