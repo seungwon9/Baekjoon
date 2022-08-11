586fa9794ed65c4db2f8e4fb4793e0e7eaa93042
tlstmddnjs <97091772+tlstmddnjs@users.noreply.github.com>
2022-08-12 00:34:52 +0900

    [Bronze III] Title: 최소, 최대, Time: 524 ms, Memory: 92168 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -10,14 +10,21 @@ public class Main {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
 		StringTokenizer st=new StringTokenizer(br.readLine());
-		int a,max=-1000001,min=1000001;
+		int arr[]=new int[n];
+		
 		for(int i=0;i<n;i++) {
-			a=Integer.parseInt(st.nextToken());
-			if(a>max)
-				max=a;
-			if(a<min)
-				min=a;
+			arr[i]=Integer.parseInt(st.nextToken());
 		}
-		System.out.println(min+" "+max);
+		int a=arr[0];
+		for(int i=1;i<n;i++) {
+			if(arr[i]>a)
+				a=arr[i];
+		}
+		int b=arr[0];
+		for(int i=1;i<n;i++) {
+			if(arr[i]<b)
+				b=arr[i];
+		}
+		System.out.println(b+" "+a);
 	}
 }
